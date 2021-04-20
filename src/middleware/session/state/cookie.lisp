1b4344f5457134cbf39cae97903af26f287c0516
madosuki <yurikasamaaikatsu@gmail.com>
2021-04-20 21:42:20 +0900

    change defalut value in samesite

--- src/middleware/session/state/cookie.lisp
+++ src/middleware/session/state/cookie.lisp
@@ -27,7 +27,7 @@
   (secure nil :type boolean)
   (httponly nil :type boolean)
   (cookie-key "lack.session" :type string)
-  (samesite 'lax :type samesite-type))
+  (samesite nil :type samesite-type))
 
 (defmethod extract-sid ((state cookie-state) env)
   (let ((req (make-request env)))
