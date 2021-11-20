cd96686fa8aa07907212afa77b3b96786dd598a3
josh <k1llfrenzy@Hotmail.co.uk>
2021-11-20 18:18:41 +0000

    modified use of feature check

--- src/middleware/session/store/memory.lisp
+++ src/middleware/session/store/memory.lisp
@@ -18,38 +18,22 @@ middleware
 ;;;for SBCL use the synchronized hash table rather than manually grabbing and releasing
 ;;;a lock on each call.
 
-#+sbcl
 (defstruct (memory-store (:include store))
-  (stash (make-hash-table :test 'equal :synchronized t)))
+  #+sbcl (stash (make-hash-table :test 'equal :synchronized t))
+  #-sbcl (stash (make-hash-table :test 'equal))
+  #-sbcl (lock (bt:make-lock "Lock for session store")))
 
-#-sbcl
-(defstruct (memory-store (:include store))
-  (stash (make-hash-table :test 'equal))
-  (lock (bordeaux-threads:make-lock "session store lock")))
-
-#+sbcl
 (defmethod fetch-session ((store memory-store) sid)
-  (gethash sid (memory-store-stash store)))
+  #+sbcl (gethash sid (memory-store-stash store))
+  #-sbcl (bt:with-lock-held ((memory-store-lock store))
+           (gethash sid (memory-store-stash store))))
 
-#-sbcl
-(defmethod fetch-session ((store memory-store) sid)
-  (bordeaux-threads:with-lock-held ((memory-store-lock store))
-    (gethash sid (memory-store-stash store))))
-
-#+sbcl
 (defmethod store-session ((store memory-store) sid session)
-  (setf (gethash sid (memory-store-stash store)) session))
+  #+sbcl (setf (gethash sid (memory-store-stash store)) session)
+  #-sbcl (bt:with-lock-held ((memory-store-lock store))
+           (setf (gethash sid (memory-store-stash store)) session)))
 
-#-sbcl 
-(defmethod store-session ((store memory-store) sid session)
-  (bordeaux-threads:with-lock-held ((memory-store-lock store))
-    (setf (gethash sid (memory-store-stash store)) session)))
-
-#+sbcl
 (defmethod remove-session ((store memory-store) sid)
-  (remhash sid (memory-store-stash store)))
-
-#-sbcl
-(defmethod remove-session ((store memory-store) sid)
-  (bordeaux-threads:with-lock-held ((memory-store-lock store))
-    (remhash sid (memory-store-stash store))))
+  #+sbcl (remhash sid (memory-store-stash store))
+  #-sbcl (bt:with-lock-held ((memory-store-lock store))
+           (remhash sid (memory-store-stash store))))
